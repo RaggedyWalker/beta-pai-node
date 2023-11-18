17f4423947dcf0f3816c5644fa07355f0916bd50
zhuyl <zhuyl01@rabyte.cn>
2023-11-18 16:36:01 +0800

    feat:  improve TypeScript eslint and prettier configurations

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,46 @@
 module.exports = {
+  root: true,
   env: {
-    'browser': false,
-    'commonjs': true,
-    'es2021': true,
-    'node': true
+    commonjs: true,
+    es2021: true,
+    node: true
   },
-  extends: 'eslint:recommended',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier', // 使用prettier中的样式规则
+    'plugin:prettier/recommended' // 启动eslint-plugin-prettier并且将prettier作为eslint的规则
+  ],
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json']
+  },
   globals: {
-    'exampleGlobalVariable': true
+    exampleGlobalVariable: true
   },
   overrides: [
     {
-      'env': {
-        'node': true
+      env: {
+        es2021: true,
+        node: true
       },
-      'files': [
-        '.eslintrc.{js,cjs}'
-      ],
-      'parserOptions': {
-        'sourceType': 'script'
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script'
       }
     }
   ],
-  parserOptions: {
-    'ecmaVersion': 'latest'
-  },
   rules: {
-    'indent': [
-      'error',
-      2
-    ],
-    'linebreak-style': [
-      'error',
-      'unix'
-    ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'always'
-    ],
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
     'quote-props': 0,
     'no-multiple-empty-lines': 2,
-    'comma-spacing': [2, { 'before': false, 'after': true }]
+    'comma-spacing': [2, { before: false, after: true }]
   }
 };
